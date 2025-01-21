0a420e285ff62d028d38d677b14d4c04e57e72cb
diaowinner <diaowinner@qq.com>
2025-01-21 20:25:41 +0800

    Add some basic functions to make it work.

--- liblintel.py
+++ liblintel.py
@@ -13,30 +13,55 @@ class Lintel:
             "⿲⿳",  # IDCs followed by 3 chars/IDSs
         ]
         for i in range(len(__MAGIC_LIST)):
-            if char in __MAGIC_LIST:
-                return i
-        raise KeyError("Not an IDS in __MAGIC_LIST.")
+            if char in __MAGIC_LIST[i]:
+                return i + 1
+        return 0
 
-    def __init__(self, *sequence) -> None:  # force-creating, danger as fuck
-        self.idc = sequence[0]
+    def __init__(
+        self, *sequence, notProcessed=""
+    ) -> None:  # force-creating, danger as fuck
+        self.idc = sequence[0] if len(sequence) > 0 else ""
         self.chars = sequence[1:]
-
-    def __str__(self) -> str:
-        return self.idc + "".join(self.chars)
+        self.notProcessed = notProcessed
 
     def fromIDS(self, ids):
-        pass
+        firstChar = ids[0]
+        loop = self._locateMagicList(firstChar)
+        match loop:
+            case -1:
+                # todo: {#}
+                self.chars = tuple(firstChar)
+                self.notProcessed = ids[1:]
+            case _:
+                remainCharList = []
+                remainChars = ids[1:]
+                for _ in range(loop):
+                    remainCharList.append(Lintel(remainChars))
+                    remainChars = remainCharList[-1].notProcessed
+                    self.idc = firstChar
+                print(remainCharList, remainChars)
+                self.chars = tuple(remainCharList)
+                self.notProcessed = remainChars
+
+    def __str__(self) -> str:
+        return self.idc + "".join(str(i) for i in self.chars)
 
 
 def _test():
 
-    def _debug(*text, lf=False):
+    def _debug(text, lf=False):
         import sys
 
         end = "" if lf else "\n"
         print(text, file=sys.stderr, end=end)
 
     lintel = Lintel("1", "2", "3")
+    _debug(lintel.idc)
+    _debug(lintel.chars)
+    _debug(str(lintel))
+
+    lintel = Lintel()
+    lintel.fromIDS("⿱僃心")
     _debug(lintel.idc)
     _debug(lintel.chars)
     _debug(str(lintel))
