cfadb14ce607b82701c7ef7b8b5a78320d80b858
diaowinner <diaowinner@qq.com>
2025-01-22 12:10:49 +0800

    Bugfix.

--- liblintel.py
+++ liblintel.py
@@ -4,47 +4,56 @@
 # (c) 2025 DWNfonts.
 
 
+def _locateMagicList(char):
+    __MAGIC_LIST = [  # everytime I name the list/dict as magic
+        "⿾⿿〾",  # IDCs followed by 1 char/IDS
+        "⿰⿱⿴⿵⿶⿷⿸⿹⿺⿼⿽㇯",  # IDCs followed by 2 chars/IDSs
+        "⿲⿳",  # IDCs followed by 3 chars/IDSs
+    ]
+    for i in range(len(__MAGIC_LIST)):
+        if char in __MAGIC_LIST[i]:
+            return i + 1
+    return 0
+
+
 class Lintel:
 
-    def _locateMagicList(self, char):
-        __MAGIC_LIST = [  # everytime I name the list/dict as magic
-            "⿾⿿〾",  # IDCs followed by 1 char/IDS
-            "⿰⿱⿴⿵⿶⿷⿸⿹⿺⿼⿽㇯",  # IDCs followed by 2 chars/IDSs
-            "⿲⿳",  # IDCs followed by 3 chars/IDSs
-        ]
-        for i in range(len(__MAGIC_LIST)):
-            if char in __MAGIC_LIST[i]:
-                return i + 1
-        return 0
-
     def __init__(
-        self, *sequence, notProcessed=""
+        self, idc, chars, notProcessed=""
     ) -> None:  # force-creating, danger as fuck
-        self.idc = sequence[0] if len(sequence) > 0 else ""
-        self.chars = sequence[1:]
+        self.idc = idc
+        self.chars = chars
         self.notProcessed = notProcessed
 
-    def fromIDS(self, ids):
-        firstChar = ids[0]
-        loop = self._locateMagicList(firstChar)
-        match loop:
-            case -1:
-                # todo: {#}
-                self.chars = tuple(firstChar)
-                self.notProcessed = ids[1:]
-            case _:
-                remainCharList = []
-                remainChars = ids[1:]
-                for _ in range(loop):
-                    remainCharList.append(Lintel(remainChars))
-                    remainChars = remainCharList[-1].notProcessed
-                    self.idc = firstChar
-                print(remainCharList, remainChars)
-                self.chars = tuple(remainCharList)
-                self.notProcessed = remainChars
+    def fromIDS(self, ids) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"Lintel<{ self.idc + "".join([str(x) for x in self.chars]) }>"
 
     def __str__(self) -> str:
-        return self.idc + "".join(str(i) for i in self.chars)
+        return self.idc + "".join([str(x) for x in self.chars])
+
+
+def importIDS(ids) -> Lintel:
+    firstChar = ids[0]
+    loop = _locateMagicList(firstChar)
+    match loop:
+        case 0:
+            idc = ""
+            # todo: {#}
+            chars = tuple(firstChar)
+            notProcessed = ids[1:]
+        case _:
+            idc = firstChar
+            remainCharList = []
+            remainChars = ids[1:]
+            for _ in range(loop):
+                remainCharList.append(importIDS(remainChars))
+                remainChars = remainCharList[-1].notProcessed
+            chars = tuple(remainCharList)
+            notProcessed = remainChars
+    return Lintel(idc, chars, notProcessed=notProcessed)
 
 
 def _test():
@@ -55,16 +64,10 @@ def _test():
         end = "" if lf else "\n"
         print(text, file=sys.stderr, end=end)
 
-    lintel = Lintel("1", "2", "3")
+    lintel = importIDS("⿺辶⿳穴⿲月⿱⿲幺言幺⿲長馬長刂心")
+    _debug(lintel)
     _debug(lintel.idc)
     _debug(lintel.chars)
-    _debug(str(lintel))
-
-    lintel = Lintel()
-    lintel.fromIDS("⿱⿰号官心")
-    _debug(lintel.idc)
-    _debug(lintel.chars)
-    _debug(str(lintel))
 
 
-# if __name__ == "__main__": _test()
+if __name__ == "__main__": _test()
