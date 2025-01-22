04e6d8ab00aa19d470bd5912c665a75abb11dc2b
diaowinner <diaowinner@qq.com>
2025-01-22 21:29:57 +0800

    Debug for half a day, finally fix it.

--- liblintel.py
+++ liblintel.py
@@ -19,44 +19,63 @@ def _locateMagicList(char):
 class Lintel:
 
     def __init__(
-        self, idc: None | str, chars: tuple, notProcessed=""
+        self, idc: None | str, chars: tuple, notProcessed="", sources=[]
     ) -> None:  # force-creating, danger as fuck
         self.idc = idc
         self.chars = chars
         self.notProcessed = notProcessed
+        self.sources = sources
 
     def __repr__(self) -> str:
-        if self.idc is str:
-            return f"Lintel<{ self.idc + "".join([str(x) for x in self.chars]) }+{ self.notProcessed }>"
+        if isinstance(self.idc, str):
+            return (
+                f"Lintel<{ self.idc + "".join([str(x) for x in self.chars]) }+{ self.notProcessed }>"
+                if self.notProcessed != ""
+                else f"Lintel<{ self.idc + ''.join([str(x) for x in self.chars]) }>"
+            )
         else:
-            return f"Lintel<{ ''.join([str(x) for x in self.chars]) }+{ self.notProcessed }>"
+            return (
+                f"Lintel<{ ''.join([str(x) for x in self.chars]) }+{ self.notProcessed }>"
+                if self.notProcessed != ""
+                else f"Lintel<{ ''.join([str(x) for x in self.chars]) }>"
+            )
 
     def __str__(self) -> str:
-        if self.idc is str:
+        if isinstance(self.idc, str):
             return self.idc + "".join([str(x) for x in self.chars]) + self.notProcessed
         else:
             return "".join([str(x) for x in self.chars]) + self.notProcessed
 
 
-def importIDS(ids) -> Lintel:
+def importIDS(ids) -> Lintel | str:
+    if len(ids) == 0:
+        return ""
     firstChar = ids[0]
     loop = _locateMagicList(firstChar)
     match loop:
         case 0:
-            idc = None
-            # todo: {#}
-            chars = tuple(firstChar)
-            notProcessed = ids[1:]
+            return firstChar
         case _:
             idc = firstChar
             remainCharList = []
             remainChars = ids[1:]
             for _ in range(loop):
-                remainCharList.append(importIDS(remainChars))
-                remainChars = remainCharList[-1].notProcessed
+                lintel = importIDS(remainChars)
+                if isinstance(lintel, Lintel):
+                    noNotProcessed = Lintel(lintel.idc, lintel.chars)
+                    remainCharList.append(noNotProcessed)
+                    remainChars = (
+                        lintel.notProcessed
+                        if isinstance(remainCharList[-1], Lintel)
+                        else remainChars[1:]
+                    )
+                else:
+                    if lintel != "":
+                        remainCharList.append(lintel)
+                    remainChars = remainChars[1:]
             chars = tuple(remainCharList)
-            notProcessed = remainChars
-    return Lintel(idc, chars, notProcessed=notProcessed)
+            print(Lintel(idc, chars, notProcessed=remainChars))
+            return Lintel(idc, chars, notProcessed=remainChars)
 
 
 def _test():
@@ -67,10 +86,10 @@ def _test():
         end = "" if lf else "\n"
         print(text, file=sys.stderr, end=end)
 
-    lintel = importIDS("⿺辶⿳穴⿲月⿱⿲幺言幺⿲長馬長刂心")
-    _debug(lintel)
-    _debug(lintel.idc)
+    lintel = importIDS("⿺辶⿳穴⿲月⿱⿲幺言幺⿲长马长刂心")
+    _debug([lintel])
     _debug(lintel.chars)
+    _debug(lintel.notProcessed)
 
 
 # if __name__ == "__main__": _test()
